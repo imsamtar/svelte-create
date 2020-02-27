382ea4f8a0de56d5e9acf1b58cffa914ce3b7b96
SAMEER TARIQ <imsamtar@gmail.com>
2020-02-27 14:36:24 +0500

    Optimized and simplified

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 chmod +777 start.sh
 cp start.sh ~/.local/bin/svelte-create
-echo "Now you can run command svelte-create in any empty directory to setup a new project"
+echo
+echo -e "> Now you can run command \033[1m\e[32msvelte-create \033[0m\e[39min any empty directory to setup a new project 🙌"
+echo
--- start.sh
+++ start.sh
@@ -1,113 +1,45 @@
 npx degit sveltejs/template-webpack .
 npm install
 npm i -D @fullhuman/postcss-purgecss postcss postcss-load-config svelte-preprocess tailwindcss
-npx tailwind init
-
-#############################################################################
-echo "Updating postcss.config.js..."
-echo "const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
-
-  whitelistPatterns: [/svelte-/],
-
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
-
-const production = !process.env.ROLLUP_WATCH
-
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    ...(production ? [purgecss] : [])
-  ]
-};" > postcss.config.js
-#############################################################################
-echo "Creating src/Tailwind.svelte..."
-echo "<style global>
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
-</style>" > src/Tailwind.svelte
-#############################################################################
-echo "Updateing src/App.svelte..."
-echo "<script>
-	import Tailwind from './Tailwind.svelte';
-	export let name;
-</script>
-
-<style>
-	h1 {
-		@apply bg-black text-white;
-	}
-</style>
-
-<h1>Hello {name}!</h1>" > src/App.svelte
-#############################################################################
-echo "Updating src/webpack.config.js..."
-echo "const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-
-const mode = process.env.NODE_ENV || 'development';
-const prod = mode === 'production';
-
-module.exports = {
-	entry: {
-		bundle: ['./src/main.js']
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
-		},
-		extensions: ['.mjs', '.js', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: __dirname + '/public',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						emitCss: true,
-						hotReload: true,
-						preprocess: require('svelte-preprocess')({ postcss: true })
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					/**
-					 * MiniCssExtractPlugin doesn't support HMR.
-					 * For developing, use 'style-loader' instead.
-					 * */
-					prod ? MiniCssExtractPlugin.loader : 'style-loader',
-					'css-loader'
-				]
-			}
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	devtool: prod ? false: 'source-map'
-};
-" > webpack.config.js
+npx tailwind init --full
 
 #############################################################################
 clear
+read -p "Enter package name (small letters): " pkgname
+sed -i 's/svelte-app/'$pkgname'/g' package.json
+
 #############################################################################
+echo "Updating postcss.config.js..."
+echo -e "const purgecss = require('@fullhuman/postcss-purgecss')({
+\tcontent: ['./src/**/*.html', './src/**/*.svelte'],
+\twhitelistPatterns: [/svelte-/],
+\tdefaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+const production = process.env.NODE_ENV==='production';
+module.exports = {
+\tplugins: [
+\t\trequire('tailwindcss'),
+\t\t...(production ? [purgecss] : [])
+\t]
+};" > postcss.config.js
+
+#############################################################################
+echo "Creating src/Tailwind.svelte..."
+echo -e "<style global>
+\t@tailwind base;
+\t@tailwind components;
+\t@tailwind utilities;
+</style>" > src/Tailwind.svelte
+
+#############################################################################
+echo "Updateing src/App.svelte..."
+sed -i "s/<script>/<script>\n\timport Tailwind from \'.\/Tailwind.svelte';/g" src/App.svelte
+
+echo "Updating webpack.config.js..."
+sed -i "s/options: {/options: {\n\t\t\t\t\t\tpreprocess: require('svelte-preprocess')({ postcss: true }),/g" webpack.config.js
+
+#############################################################################
+clear
 read -p "Do you want to install sveltefire? [Y] " sveltefire
 if [ -z $sveltefire ]; then sveltefire="y"; fi
 if [ $sveltefire != "n" ] && [ $sveltefire != "N" ]; then
@@ -116,7 +48,6 @@ if [ $sveltefire != "n" ] && [ $sveltefire != "N" ]; then
 fi
 #############################################################################
 clear
-#############################################################################
 read -p "Do you want to install @conposi/gestures? [Y] " gestures
 if [ -z $gestures ]; then gestures="y"; fi
 if [ $gestures != "n" ] && [ $gestures != "N" ]; then
@@ -124,7 +55,6 @@ if [ $gestures != "n" ] && [ $gestures != "N" ]; then
 fi
 #############################################################################
 clear
-#############################################################################
 read -p "Do you want to install Cypress? [Y] " cypress
 if [ -z $cypress ]; then cypress="y"; fi
 if [ $cypress != "n" ] && [ $cypress != "N" ]; then
@@ -132,7 +62,7 @@ if [ $cypress != "n" ] && [ $cypress != "N" ]; then
         npm i -D cypress-firebase
     fi
     echo "It can take a long time! It is recommended to install cypress globally."
-    read -p "Are you sure to install cypress as a dev-dependency? [N] " cypress
+    read -p "Are you sure to install cypress as a devDependency? [N] " cypress
     if [ -z $cypress ]; then cypress="n"; fi
     if [ $cypress = "y" ] || [ $cypress = "Y" ]; then
         npm i -D cypress
@@ -140,4 +70,3 @@ if [ $cypress != "n" ] && [ $cypress != "N" ]; then
 fi
 #############################################################################
 clear
-#############################################################################
