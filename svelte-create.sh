9c165633869fec6cc29c8ef413a5b8fec8aba34a
SAMEER TARIQ <imsamtar@gmail.com>
2020-03-13 15:17:53 +0500

    Asks to setup hasura backend as an alternative to firebase

--- svelte-create.sh
+++ svelte-create.sh
@@ -60,11 +60,58 @@ sed -i "s/options: {/options: {\n\t\t\t\t\t\tpreprocess: require('svelte-preproc
 
 ####
 clear
-read -p "Do you want to install sveltefire? [Y] " sveltefire
-if [ -z $sveltefire ]; then sveltefire="y"; fi
-if [ $sveltefire != "n" ] && [ $sveltefire != "N" ]; then
-    npm i -D sveltefire
-    npm i -D firebase
+read -p "Do you want to setup for hasura? [Y] " hasura
+if [ -z $hasura ]; then hasura="y"; fi
+if [ $hasura != "n" ] && [ $hasura != "N" ]; then
+    npm i --save apollo-cache-inmemory apollo-client apollo-link apollo-link-error apollo-link-http apollo-link-ws graphql graphql-tag subscriptions-transport-ws
+    echo "import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// Create an http link:
+const httpLink = new HttpLink({
+    uri: 'http://localhost:8080/v1/graphql'
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:8080/v1/graphql',
+    options: {
+        reconnect: true
+    }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+    // split based on operation type
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink,
+);
+
+export default new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+});
+" > src/graphql-client.js
+else
+    clear
+    read -p "Do you want to install sveltefire? [Y] " sveltefire
+    if [ -z $sveltefire ]; then sveltefire="y"; fi
+    if [ $sveltefire != "n" ] && [ $sveltefire != "N" ]; then
+        npm i -D sveltefire
+        npm i -D firebase
+    fi
 fi
 ####
 clear
